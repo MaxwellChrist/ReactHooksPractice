f79981218ae731768536eec46f3bdc1b6f2e2922
MaxwellChrist <ribthulu@gmail.com>
2022-08-23 18:41:57 -0400

    practices useReduce and useRef hooks along with reducer function without redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer, useRef } from 'react'
 import Star from './components/Star';
 import Users from './components/Users';
 
+const initialState = {
+  message: "hi"
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'yell':
+      return {
+        message: `HEY! I just said ${state.message}`
+      }
+    case 'whisper':
+      return {
+        message: `excuse me, I just said ${state.message}`
+      }
+    default: 
+      return initialState
+  }
+}
+
 function App() {
+  // const sound = useRef()
+  // const color = useRef()
+  const [sound, setSound] = useState("")
+  const [color, setColor] = useState("")
   const [status, setStatus] = useState("Not Delivered")
-  const [checked, setChecked] = useState(false)
+  // const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useReducer((checked) => !checked, false)
   const [name, setName] = useState("Jan")
   const [users, setUsers] = useState([])
+  const [number, setNumber] = useReducer((number, newNumber) => number + newNumber, 0)
+  const [state, dispatch] = useReducer(
+    reducer,
+    initialState
+  )
 
   useEffect(() => {
     document.title = `Celebrate ${name}`
@@ -20,16 +49,58 @@ function App() {
     .catch(err => console.log(err))
   }, [])
 
+  // const formSubmit = (e) => {
+  //   e.preventDefault()
+  //   const soundValue = sound.current.value
+  //   const colorValue = color.current.value
+  //   let heading = document.getElementById("soundAndColor")
+  //   heading.style.disabled = false
+  //   heading.textContent = `${soundValue} sounds like ${colorValue}`
+  //   sound.current.value = ""
+  //   color.current.value = ""
+  // }
+
+  const formSubmit = (e) => {
+    e.preventDefault()
+    let heading = document.getElementById("soundAndColor")
+    heading.style.disabled = false
+    heading.textContent = `${sound} sounds like ${color}`
+    setSound("")
+    setColor("")
+  }
 
   return (
     <div className="App">
+      <h1 onClick={() => setNumber(1)}>{number}</h1>
+
       <h1>The package is: {status}</h1>
       <button onClick={() => status === "Not Delivered" ? setStatus("Delivered") : setStatus("Not Delivered")}>Deliver</button>
 
-      <input type="checkbox" value={checked} onChange={() => setChecked(!checked)}/>
+      {/* <input type="checkbox" value={checked} onChange={() => setChecked(!checked)}/>
+      <p>{checked ? "checked" : "not checked"}</p> */}
+
+      <input type="checkbox" value={checked} onChange={setChecked}/>
       <p>{checked ? "checked" : "not checked"}</p>
 
       <Star total={5} />
+
+      <p>Message: {state.message}</p>
+      <button onClick={() => dispatch({type: "yell"})}>Yell</button>
+      <button onClick={() => dispatch({type: "whisper"})}>Whisper</button>
+
+      {/* <form onSubmit={formSubmit}>
+        <h3 id="soundAndColor" style={{disabled: 'true'}}></h3>  
+        <input ref={sound} type="text" placeholder='Sound...'/>
+        <input ref={color} type="color" />
+        <button>Add</button>
+      </form> */}
+
+      <form onSubmit={formSubmit}>
+        <h3 id="soundAndColor" style={{disabled: 'true'}}></h3>  
+        <input onChange={(e) => setSound(e.target.value)} value={sound} type="text" placeholder='Sound...'/>
+        <input onChange={(e) => setColor(e.target.value)} value={color} type="color" />
+        <button>Add</button>
+      </form>
 
       <section>
         <p>Congratulations {name}!</p>
@@ -37,7 +108,7 @@ function App() {
       </section>
 
       
-      <Users data={users} />
+      <Users data={users} setData={setUsers}/>
     </div>
   );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-const Users = ({data}) => {
+const Users = ({data, setData}) => {
     if (data) {
         return (
-            <ul>
-                {data.map((item) => (
-                    <li style={{listStyleType: 'none'}} key={item.id}>{item.login}</li>
-                ))}
-            </ul>
+            <div>
+                <ul>
+                    {data.map((item) => (
+                        <li style={{listStyleType: 'none'}} key={item.id}>{item.login}</li>
+                    ))}
+                </ul>
+                <button onClick={() => setData([])}>Clear List</button>
+            </div>
         )
     } else {
         <p>No Users</p>
